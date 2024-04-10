83018e32707d012a453c2b67f52be82416224963
Alexander Stefanov <46712355+netmasterbg@users.noreply.github.com>
2024-04-10 11:13:22 +0300

    fix applications port to 8090

--- tests/ui/grades.test.js
+++ tests/ui/grades.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check grades page', async ({ page }) => {
-    await page.goto('http://localhost:8080/grades-list');
+    await page.goto('http://localhost:8090/grades-list');
     const list = await page.$('dl');
     expect(list).toBeTruthy();
   });
